f5e3fa40c0b0b811beded2207b8f6bc9ad907724
Jxx5o2 <vtsun98@gmail.com>
2025-07-07 23:24:17 +0900

    우주선 움직이기 + 우주선 소리
    AddRelativeForce와 transform.Rotate로 우주선 방향 조절 및 이동 구현, InputAction로 키보드 키 받아서 키 설정 도 구현. AudioSource로 우주선 엔진소리 구현

--- Rocket Boost/Assets/Scripts/Movement.cs
+++ Rocket Boost/Assets/Scripts/Movement.cs
@@ -1,23 +1,69 @@
 using System;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
+    [SerializeField] float thrustStrngth = 100f;
+    [SerializeField] float rotationStrngth = 100f;
+    Rigidbody rb;
+    AudioSource audioSource;
 
-    void OnEnable()
+    void Start()
     {
-        thrust.Enable();
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
-    void Update()
+    void OnEnable()             // 오브젝트가 켜질 때마다 호출됨 
+    {                           // (씬 로드 직후           
+        thrust.Enable();
+        rotation.Enable();       //  OR gameObject.SetActive(true) 했을 때 
+    }                           //  OR 스크립트 컴포넌트 체크박스 다시 켰을 때)
+
+    void FixedUpdate()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+    void ProcessThrust()
     {
         if (thrust.IsPressed())
         {
-            Debug.Log("Dang, I need some space");
+            rb.AddRelativeForce(Vector3.up * thrustStrngth * Time.fixedDeltaTime);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
         }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        if (rotationInput < 0)
+        {
+            ApplyRotation(rotationStrngth);
+        }
+        else if (rotationInput > 0)
+        {
+            ApplyRotation(-rotationStrngth);
+        }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
+        rb.freezeRotation = false;
     }
 }
 
